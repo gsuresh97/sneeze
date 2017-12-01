62ecf07f3e96c3fb439050e7597429f5b8125866
Hannah <HannaHojaiji@users.noreply.github.com>
2017-11-30 17:42:45 -0800

    Gallery

---
+++ sneeze_app/src/components/gallery.js
@@ -0,0 +1,21 @@
+import Gallery from 'react-native-photo-gallery';
+
+class Gallery extends React.Component {
+    render() {
+        const data = [
+            {
+                id: 'first image',
+                image: require('./yourImage.jpg'),
+                thumb: require('./yourImageThumb.jpg'),
+                overlay: <Overlay />
+    },
+        {
+            id: 'Second image',
+                image: require('./yourImage2.jpg'),
+            thumb: require('./yourImageThumb2.jpg'),
+            overlay: <OtherOverlay />
+        }
+    ];
+
+        return <Gallery data={data} />;
+    }
