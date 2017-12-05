d2b6c2f4e46d43aa13091a027a394b0b7f889886
Austin Berke <austinberke@gmail.com>
2017-12-05 14:13:33 -0800

    Added getData endpoint

--- sneeze_backend/sneezeApi/urls.py
+++ sneeze_backend/sneezeApi/urls.py
@@ -4,4 +4,5 @@ from sneezeApi import views
 urlpatterns = [
 	url(r'^sneeze/$', views.sneeze),
 	url(r'^getMemes/$', views.getMemes),
+	url(r'^getData/$', views.getData),
 ]
--- sneeze_backend/sneezeApi/views.py
+++ sneeze_backend/sneezeApi/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from pymongo import MongoClient, GEO2D
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
+from django.conf import settings
 import os
 from time import time
 from bson.son import SON
@@ -36,26 +37,24 @@ col = db.info
 @api_view(['POST'])
 def sneeze(request):
     if request.method == 'POST':
-        data = request.data
+        # data = request.data
         col.create_index( [("loc", GEO2D)] )
 
         # p = BSON.encode({"picture": data["data"].read()})
         name = str(uuid.uuid4())
-        f = open(name, "w+")
-        print type(data["data"])
-        f.write(str(data["data"]))
-        path = default_storage.save(name, ContentFile(data["data"]))
+
+        path = default_storage.save(name, ContentFile(request.FILES['data'].read()))
         tmp_file = os.path.join(settings.MEDIA_ROOT, path)
-        f.close()
+
         post = {
             # is this the correct format for accessing attributes
             # from the POST request? we might need to do additional
             # parsing
-            "loc": [float(data["longitude"]), float(data["latitude"])],
-            "format": data["format"],
-            "path": tmp_file, # https://docs.mongodb.com/manual/reference/bson-types/
-            "user": data["user"],
-            "radius": float(data["rad"]),
+            "loc": [float(request.POST.get("longitude")), float(request.POST.get("latitude"))],
+            "format": request.POST.get("format"),
+            "path": name, # https://docs.mongodb.com/manual/reference/bson-types/
+            "user": request.POST.get("user"),
+            "radius": float(request.POST.get("rad")),
             "time": time(),
             "ids": []
         }
@@ -73,17 +72,26 @@ def getMemes(request):
         query = {"loc": {"$within": {"$center": [[float(data["longitude"]), float(data["latitude"])], 1000]}}}
         print col.find(query).count()
         pic = None
+        ids = []
         for m in col.find(query):
             if data["id"] not in m["ids"]:
                 pic = m
-                break
+                ids.append(pic["path"])
         name = m["path"]
         f = open(name, "r")
         res = {
             "latitude": pic["loc"][1],
             "longitude": pic["loc"][0],
-            "data": f.read()
+            "ids": ids
 
         }
         return JsonResponse(res)
+    return HttpResponse(status=501)
+
+@api_view(['POST'])
+def getData(request):
+    if request.method == 'POST':
+        f = open(request.POST.get('id'), "r")
+        res = HttpResponse(f.read(), content_type="image/png")
+        return res
     return HttpResponse(status=501)
